e94acb7e68518f641b7b39b4a33e69662aa42cd2
Juan Guevara <Juan Guevara>
2020-09-27 17:54:29 -0600

    Deploy

--- client/src/App.vue
+++ client/src/App.vue
@@ -7,7 +7,6 @@
         <b-collapse id="nav-collapse" is-nav>
           <b-navbar-nav>
             <b-nav-item to="/">Home</b-nav-item>
-            <b-nav-item v-if="$auth.isAuthenticated" to="/profile">Profile</b-nav-item>
           </b-navbar-nav>
 
           <b-navbar-nav class="ml-auto">
